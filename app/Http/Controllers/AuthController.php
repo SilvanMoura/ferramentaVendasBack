7bd835db1dfcecc0cfc1fead73143dab1bbd5f73
Silvan Moura <silvancortesmoura@outlook.com>
2023-08-23 16:17:09 -0300

    Import adjustments

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
 class AuthController extends Controller
 {
     /**
