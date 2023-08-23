12941dd6ac6a4116dbc63d3a145f2652560e4c0a
Silvan Moura <silvancortesmoura@outlook.com>
2023-08-23 14:31:34 -0300

    Defining validations for login and registration

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegsiterRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class RegsiterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'string|required',
+            'email' => 'email|required',
+            'password' => 'string|required',
         ];
     }
 }
